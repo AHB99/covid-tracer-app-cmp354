fbab01f6990b88a001e09394b838ff6a71d0bc7a
Azadan <azadanhb99@gmail.com>
2020-10-16 23:35:21 +0400

    Setup Firebase test

--- app/src/main/java/com/example/cmp354_covidtracer/MainActivity.java
+++ app/src/main/java/com/example/cmp354_covidtracer/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.cmp354_covidtracer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Users");
+
+        myRef.child("User1").setValue(new Users("Johnathan"));
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Users value = dataSnapshot.child("User1").getValue(Users.class);
+                Toast.makeText(MainActivity.this, value.getName(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/cmp354_covidtracer/Users.java
@@ -0,0 +1,22 @@
+package com.example.cmp354_covidtracer;
+
+public class Users {
+    public Users() {
+    }
+
+    public Users(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+
+}
